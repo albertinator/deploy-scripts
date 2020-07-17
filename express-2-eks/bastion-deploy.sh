c2da7fd8103326734f879364b4a87d91081e17cb
Albert Ho <ah323@cornell.edu>
2020-07-16 21:49:12 -0700

    Add bastion host creation scripts

---
+++ express-2-eks/bastion-deploy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Install AWS CLI: https://aws.amazon.com/cli
+# Configure profile for AWS CLI:
+# $ aws configure --profile <profile-name>
+
+export AWS_PROFILE="profile_name"
+export CLUSTER_NAME="cluster_name"
+
+# create key pair
+aws ec2 create-key-pair \
+  --profile ${AWS_PROFILE} \
+  --key-name ${CLUSTER_NAME}-bastion \
+  --query 'KeyMaterial' \
+  --output text > ${CLUSTER_NAME}-bastion.pem
+chmod 400 ${CLUSTER_NAME}-bastion.pem
+
+# get cluster VPC ID
+export VPC_ID=$(
+  aws ec2 describe-vpcs \
+    --profile ${AWS_PROFILE} \
+    --filters "Name=tag:alpha.eksctl.io/cluster-name,Values=${CLUSTER_NAME}" \
+    --query 'Vpcs[0].VpcId' \
+    --output text
+)
+
+# get all existing security groups for this VPC whose name includes the cluster name
+export CLUSTER_SGS=$(
+  aws ec2 describe-security-groups \
+    --profile ${AWS_PROFILE} \
+    --filters "Name=vpc-id,Values=${VPC_ID}" "Name=group-name,Values=*${CLUSTER_NAME}*" \
+    --query 'SecurityGroups[*].GroupId' \
+    --output text
+)
+
+# create security group
+export SG_ID=$(
+  aws ec2 create-security-group \
+   --profile ${AWS_PROFILE} \
+   --description "Launch SG for ${CLUSTER_NAME}-bastion" \
+   --group-name launch-${CLUSTER_NAME}-bastion \
+   --vpc-id ${VPC_ID} \
+   --query 'GroupId' \
+   --output text
+)
+
+# add inbound rule to security group
+# for opening SSH (TCP port 22), allowing only current IP
+aws ec2 authorize-security-group-ingress \
+  --group-id ${SG_ID} \
+  --protocol tcp \
+  --port 22 \
+  --cidr $(curl icanhazip.com)/32
+
+# get a public subnet ID in the VPC
+export SUBNET_ID=$(
+  aws ec2 describe-subnets \
+    --profile ${AWS_PROFILE} \
+    --filters "Name=vpc-id,Values=${VPC_ID}" "Name=tag:Name,Values=*Public*" \
+    --query 'Subnets[0].SubnetId' \
+    --output text
+)
+
+export INSTANCE_ID=$(
+  aws ec2 run-instances \
+    --profile ${AWS_PROFILE} \
+    --image-id ami-0a0ddd875a1ea2c7f \
+    --count 1 \
+    --instance-type t2.micro \
+    --key-name ${CLUSTER_NAME}-bastion \
+    --security-group-ids ${SG_ID} ${CLUSTER_SGS} \
+    --subnet-id ${SUBNET_ID} \
+    --associate-public-ip-address \
+    --user-data file://bastion-setup.sh \
+    --query 'Instances[0].InstanceId' \
+    --output text
+)
+
+# get PublicDnsName from running instance
+export PUBLIC_DNS_NAME=$(
+  aws ec2 describe-instances \
+    --profile ${AWS_PROFILE} \
+    --instance-ids ${INSTANCE_ID} \
+    --query 'Reservations[0].Instances[0].PublicDnsName' \
+    --output text
+)
+
+echo $PUBLIC_DNS_NAME
+# ssh -i ${CLUSTER_NAME}-bastion.pem ubuntu@${PUBLIC_DNS_NAME}
---
+++ express-2-eks/bastion-setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Script to set up Bastion host within VPC
+# with kubectl and eksctl commands
+# This script is run as root when passed to `aws ec2 run-instances`
+
+export CLUSTER_NAME="cluster_name"
+export REGION="us-east-1"
+
+apt update
+
+apt install -y awscli
+aws configure
+
+# install aws-iam-authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.7/2020-07-08/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws-iam-authenticator help
+
+# install aws v2 if not already
+apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+# install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# install kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.7/2020-07-08/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin
+kubectl version --short --client
+
+# ask user to enter AWS credentials upon login to SSH session
+runuser -l ubuntu -c 'echo aws configure >> ~/.bashrc'
+runuser \
+  -l ubuntu \
+  -c 'echo eksctl utils write-kubeconfig --cluster '${CLUSTER_NAME}' --region '${REGION}' >> ~/.bashrc'
+
+# securely delete AWS and Kube credentials upon logout from SSH session
+runuser -l ubuntu -c 'echo rm -rf ~/.aws >> ~/.bash_logout'
+runuser -l ubuntu -c 'echo rm -rf ~/.kube >> ~/.bash_logout'
+
+# securely delete AWS and Kube credentials every 15 mins (in case .bash_logout is not invoked)
+echo "*/5 * * * * rm -rf ~/.aws" >> ubuntu
+echo "*/5 * * * * rm -rf ~/.kube" >> ubuntu
+chmod 600 ubuntu
+chown ubuntu:crontab ubuntu
+mv ubuntu /var/spool/cron/crontabs/ubuntu
